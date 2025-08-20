4f412a84328b102fd91906560a280b1a5ab97e22
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-08-20 17:18:20 +0100

    Konga sales app development

---
+++ konga_app/script.js
@@ -0,0 +1,42 @@
+let slideIndex = 1;
+
+displayImages();
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+
+}
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+
+}
+function showSlides(n) {
+    let index;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (index = 0; index < slides.length; index++) {
+        slides[index].style.display = "none";
+    }
+    for (index = 0; index < dots.length; index++) {
+        dots[index].className = dots[index].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+
+}
+function displayImages() {
+    let index = 0;
+    const images = document.getElementsByClassName("mySlides");
+    for (index = 0; index < images.length; index++) {
+        images[index].style.display = "none";
+    }
+    index++;
+    if (index > images.length) {
+        index = 1;
+    }
+    images[index-1].style.display = "block";
+    setTimeout(displayImages, 2000);
+}
