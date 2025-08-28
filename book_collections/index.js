0758af3447926623e4db3dbd29799de7b3fd7cbd
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-08-28 17:04:01 +0100

    index file for book collections app

---
+++ book_collections/index.js
@@ -0,0 +1,12 @@
+const wrapper = document.getElementById("wrapper");
+// console.log(wrapper);
+
+const titles = document.getElementsByClassName("title");
+// console.log(titles);
+
+const list = document.getElementsByTagName("li")
+// console.log(Array.isArray(list));
+// console.log(Array.from(list));
+
+const ul = document.querySelectorAll("li")
+console.log(ul)
