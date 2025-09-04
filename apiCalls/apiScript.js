7005d91ed17510702f73e5f1331b081de7cc8018
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-09-04 16:42:16 +0100

    style sheet for managing products display user interface

---
+++ apiCalls/apiScript.js
@@ -0,0 +1,40 @@
+const PRODUCT_URL = "https://fakestoreapi.com/products";
+
+// const getProducts = (url) => {
+//     fetch(url)
+//         .then(response => response.json())
+//         .then(data => console.log(data))
+//         .catch(error => console.error(error));
+// };
+
+// getProducts(PRODUCT_URL);
+
+const getProducts = async (url) => {
+    try{
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+        displayProducts(data);
+    }catch(error){
+        console.error(error);
+    }
+};
+
+getProducts(PRODUCT_URL);
+
+const productTag = document.querySelector(".products");
+
+const displayProducts = (products) => {
+    products.forEach(product => {
+        const {image, title, price} = product;
+        const productWrapper = document.createElement("div");
+        productWrapper.innerHTML = `
+        <a href="#">
+          <img src="${image}" alt="">
+          <p>Title: ${title}</p>
+          <p>Price: &#8358; ${price}</p>
+        </a>`
+        productWrapper.classList.add("images");
+        productTag.appendChild(productWrapper)
+    })
+}
